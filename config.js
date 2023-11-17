f717764362a04a799634ece20e3e4de9a5069211
15730654943 <75407494+15730654943@users.noreply.github.com>
2023-11-17 14:15:00 +0800

    Update config.js

--- config.js
+++ config.js
@@ -3,4 +3,4 @@ module.exports = {
   model: 'KB2000',
   version: '9.0.6',
   pubKey: 'MIGfMA0GCSqGSIb3DQEBAQUAA4GNADCBiQKBgQCZLyV4gHNDUGJMZoOcYauxmNEsKrc0TlLeBEVVIIQNzG4WqjimceOj5R9ETwDeeSN3yejAKLGHgx83lyy2wBjvnbfm/nLObyWwQD/09CmpZdxoFYCH6rdDjRpwZOZ2nXSZpgkZXoOBkfNXNxnN74aXtho2dqBynTw3NFTWyQl8BQIDAQAB',
-};
+}; 
